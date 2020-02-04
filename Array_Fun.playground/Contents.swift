4242e576513ea31f45730ad11db396f85ec0fc1d
gabrielespinosa938 <Gabriel.espinosa936@myci.csuci.edu>
2020-02-03 22:09:28 -0800

    Binary Search

---
+++ Array_Fun.playground/Contents.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+var numbers : [Int] = []
+var populateArray = 0
+
+while populateArray < 10
+{
+    let randomInt = Int.random(in: 5...60)
+    numbers.append(randomInt)
+    populateArray = populateArray + 1
+}
+
+print(numbers)
+
+var tempArray : [Int] = []
+while numbers.count != 0
+{
+    tempArray.append(numbers.removeLast())
+}
+print(tempArray)
+
+
+func binarySearch(array : [Int], target : Int) -> Bool
+{
+    let startIndex = 0
+    let endIndex = array.count - 1
+    let middleIndex = endIndex/2
+    let middleValue = array[middleIndex]
+    
+    if array.count == 0 || target < array[startIndex] || target > array[endIndex]
+    {
+        return false
+    }
+    if target < middleValue
+    {
+        let slice = Array(array[startIndex]...middleIndex - 1)
+        return binarySearch(array: slice, target: target)
+    }
+    if target > middleValue
+    {
+        let slice = Array(middleIndex + 1...array[endIndex])
+        return binarySearch(array: slice, target: target)
+    }
+    if target == middleValue
+    {
+        return true
+    }
+    
+    return true
+}
+
+let myArray = [2,5,65,32,45,9,1,34,3]
+binarySearch(array: myArray, target: 8)
