897f60f4baba7d6f34f4fc02fa16ea0249448720
gabrielespinosa938 <Gabriel.espinosa936@myci.csuci.edu>
2020-02-24 09:28:49 -0800

    unique Conversion

--- Conversion.playground/Contents.swift
+++ Conversion.playground/Contents.swift
@@ -1,26 +1,25 @@
 import UIKit
 
-//let letters = "string"
-//let middle = letters.index(letters.startIndex, offsetBy: letters.count / 2)
-//
-//for index in letters.indices {
-//
-//    // to traverse to half the length of string
-//    if index == middle { break }  // s, t, r
-//
-//    print(letters[index])  // s, t, r, i, n, g
-//}
 
 
-let s = "schooliscool"
-var arrayOfAsciiValues : [UInt8] = []
-for i in s.indices[s.startIndex..<s.endIndex] {
-    arrayOfAsciiValues.append(Character("\(s[i])").asciiValue!)
-}
-
-print(arrayOfAsciiValues)
-
-for i in 0..<arrayOfAsciiValues.count
+func uniqueCharToAsciiConversion(string : String) -> Bool
 {
-    print(arrayOfAsciiValues[i])
+    var arrayOfAsciiValues : [UInt8] = []
+    for i in string.indices[string.startIndex..<string.endIndex]
+    {
+        arrayOfAsciiValues.append(Character("\(string[i])").asciiValue!)
+    }
+    arrayOfAsciiValues.sorted()
+    for i in 0..<arrayOfAsciiValues.count - 1
+    {
+        if arrayOfAsciiValues[i] == arrayOfAsciiValues[i+1]
+        {
+            return false
+        }
+    }
+    return true
 }
+let word = "This "
+word.trimmingCharacters(in: .whitespaces)
+uniqueCharToAsciiConversion(string: word)
+
