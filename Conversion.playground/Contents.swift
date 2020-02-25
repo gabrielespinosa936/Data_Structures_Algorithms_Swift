2dc78a52c02efc574f19818a4240236306e84810
gabrielespinosa938 <Gabriel.espinosa936@myci.csuci.edu>
2020-02-25 10:11:43 -0800

    Will finish the removeDuplicates function after class

--- Conversion.playground/Contents.swift
+++ Conversion.playground/Contents.swift
@@ -23,3 +23,76 @@ let word = "This "
 word.trimmingCharacters(in: .whitespaces)
 uniqueCharToAsciiConversion(string: word)
 
+func isPalindrom(string : String) -> Bool
+{
+    var set : Set<Character> = []
+    for char in string
+    {
+        if set.contains(char) {
+            set.remove(char)
+        }else{
+            set.insert(char)
+        }
+    }
+    return set.count <= 1
+}
+
+isPalindrom(string: "racecar")
+
+class Node
+{
+    var value : Int
+    var next : Node?
+    init(value : Int, next : Node?)
+    {
+        self.value = value
+        self.next = next
+    }
+}
+
+class LinkedList
+{
+    var head : Node?
+    
+    func insertNode(value : Int)
+    {
+        if head == nil
+        {
+            head = Node(value: value, next: nil)
+            return
+        }
+        var currentNode = head
+        while currentNode?.next != nil {
+            currentNode = currentNode?.next
+        }
+        currentNode?.next = Node(value: value, next: nil)
+    }
+    func displayList()
+    {
+        var currentNode = head
+        while currentNode != nil {
+            print(currentNode?.value ?? "")
+            currentNode = currentNode?.next
+        }
+        
+    }
+    func removeDuplicates()
+    {
+        var currentNode = head
+        var fastNode : Node?
+        var previousNode : Node?
+        
+        
+    }
+}
+
+
+let list = LinkedList()
+var index = 0
+while(index < 10)
+{
+    let randomInt = Int.random(in: 0...100)
+    list.insertNode(value: randomInt)
+    index = index + 1
+}
+list.displayList()
